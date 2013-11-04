d51ca56a82396c1e2d6919ab4f54b8534bab1190
FelixA <felixaber@gmail.com>
2013-11-04 21:30:18 +0100

    bin klassen

--- MeetInTheMiddle/gen/com/example/meetinthemiddle/R.java
+++ MeetInTheMiddle/gen/com/example/meetinthemiddle/R.java
@@ -24,18 +24,45 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080001;
+        public static final int textView1=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_display_meetings=0x7f030000;
+        public static final int activity_display_message=0x7f030001;
+        public static final int activity_display_messages=0x7f030002;
+        public static final int activity_display_profile=0x7f030003;
+        public static final int activity_display_questions=0x7f030004;
+        public static final int activity_display_settings=0x7f030005;
+        public static final int activity_main=0x7f030006;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int display_meetings=0x7f070000;
+        public static final int display_message=0x7f070001;
+        public static final int display_messages=0x7f070002;
+        public static final int display_profile=0x7f070003;
+        public static final int display_questions=0x7f070004;
+        public static final int display_settings=0x7f070005;
+        public static final int main=0x7f070006;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int button_contacts=0x7f050004;
+        public static final int button_meet=0x7f050006;
+        public static final int button_messages=0x7f050005;
+        public static final int button_profile=0x7f050007;
+        public static final int button_questions=0x7f050008;
+        public static final int button_send=0x7f050010;
+        public static final int button_settings=0x7f050009;
+        public static final int edit_message=0x7f050003;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_display_meetings=0x7f05000c;
+        public static final int title_activity_display_message=0x7f05000a;
+        public static final int title_activity_display_messages=0x7f05000b;
+        public static final int title_activity_display_profile=0x7f05000d;
+        public static final int title_activity_display_questions=0x7f05000e;
+        public static final int title_activity_display_settings=0x7f05000f;
     }
     public static final class style {
         /** 
