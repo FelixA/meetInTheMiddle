64eb091b415b7a916300a72ef0cccb980d0bc424
Levon <Levon@Levon-PC>
2013-11-20 22:19:35 +0100

    Kann man ignorieren

--- MeetInTheMiddle/gen/com/example/meetinthemiddle/R.java
+++ MeetInTheMiddle/gen/com/example/meetinthemiddle/R.java
@@ -24,29 +24,42 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int textView1=0x7f080000;
+        public static final int action_forgot_password=0x7f080008;
+        public static final int action_settings=0x7f080009;
+        public static final int email=0x7f080003;
+        public static final int login=0x7f080005;
+        public static final int login_form=0x7f080002;
+        public static final int login_status=0x7f080000;
+        public static final int login_status_message=0x7f080001;
+        public static final int password=0x7f080004;
+        public static final int sign_in_button=0x7f080006;
+        public static final int textView1=0x7f080007;
     }
     public static final class layout {
-        public static final int activity_display_meetings=0x7f030000;
-        public static final int activity_display_message=0x7f030001;
-        public static final int activity_display_messages=0x7f030002;
-        public static final int activity_display_profile=0x7f030003;
-        public static final int activity_display_questions=0x7f030004;
-        public static final int activity_display_settings=0x7f030005;
-        public static final int activity_main=0x7f030006;
+        public static final int activity_display_login=0x7f030000;
+        public static final int activity_display_meetings=0x7f030001;
+        public static final int activity_display_message=0x7f030002;
+        public static final int activity_display_messages=0x7f030003;
+        public static final int activity_display_profile=0x7f030004;
+        public static final int activity_display_questions=0x7f030005;
+        public static final int activity_display_settings=0x7f030006;
+        public static final int activity_main=0x7f030007;
     }
     public static final class menu {
-        public static final int display_meetings=0x7f070000;
-        public static final int display_message=0x7f070001;
-        public static final int display_messages=0x7f070002;
-        public static final int display_profile=0x7f070003;
-        public static final int display_questions=0x7f070004;
-        public static final int display_settings=0x7f070005;
-        public static final int main=0x7f070006;
+        public static final int display_login=0x7f070000;
+        public static final int display_meetings=0x7f070001;
+        public static final int display_message=0x7f070002;
+        public static final int display_messages=0x7f070003;
+        public static final int display_profile=0x7f070004;
+        public static final int display_questions=0x7f070005;
+        public static final int display_settings=0x7f070006;
+        public static final int main=0x7f070007;
     }
     public static final class string {
+        public static final int action_forgot_password=0x7f050016;
         public static final int action_settings=0x7f050001;
+        public static final int action_sign_in_register=0x7f050014;
+        public static final int action_sign_in_short=0x7f050015;
         public static final int app_name=0x7f050000;
         public static final int button_contacts=0x7f050004;
         public static final int button_meet=0x7f050006;
@@ -56,7 +69,17 @@ public final class R {
         public static final int button_send=0x7f050010;
         public static final int button_settings=0x7f050009;
         public static final int edit_message=0x7f050003;
+        public static final int error_field_required=0x7f05001b;
+        public static final int error_incorrect_password=0x7f05001a;
+        public static final int error_invalid_email=0x7f050018;
+        public static final int error_invalid_password=0x7f050019;
         public static final int hello_world=0x7f050002;
+        public static final int login_progress_signing_in=0x7f050017;
+        /**  Strings related to login 
+         */
+        public static final int prompt_email=0x7f050012;
+        public static final int prompt_password=0x7f050013;
+        public static final int title_activity_display_login=0x7f050011;
         public static final int title_activity_display_meetings=0x7f05000c;
         public static final int title_activity_display_message=0x7f05000a;
         public static final int title_activity_display_messages=0x7f05000b;
@@ -91,5 +114,6 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int LoginFormContainer=0x7f060002;
     }
 }
